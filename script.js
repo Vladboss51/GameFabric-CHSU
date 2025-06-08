c6c35f61b1ce86773caea9bb6139d84308b3ba58
Владислав Поляков <vlad80920@gmail.com>
2025-06-09 01:59:43 +0300

    готово...

--- script.js
+++ script.js
@@ -1,58 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-
-    // Получаем кнопку "наверх"
+    // Кнопка "наверх"
     const backToTopButton = document.querySelector('.back-to-top');
-
-    // Обработчик прокрутки страницы
     window.addEventListener('scroll', () => {
-        // Если прокрутили больше 300px, показываем кнопку
         if (window.pageYOffset > 300) {
             backToTopButton.classList.add('show');
         } else {
             backToTopButton.classList.remove('show');
         }
     });
-
-    // Обработчик клика по кнопке
     backToTopButton.addEventListener('click', () => {
-        // Плавная прокрутка вверх
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
     });
-
+    // Мобтльное меню
     const iframe = document.querySelector('iframe');
     iframe.onload = function() {
         const currentPage = window.location.pathname.split('/').pop();
         iframe.contentWindow.postMessage(currentPage, '*');
     };
-    //---------------------------------------------
-
-
-
     const menuBtn = document.getElementById('mobile-menu-btn');
     const mobileMenu = document.getElementById('mob-menu');
-    
-    // Обработчик клика по кнопке
     menuBtn.addEventListener('click', function() {
-        // Переключаем состояния
         this.classList.toggle('active');
         mobileMenu.classList.toggle('active');
-        
-        // Блокируем прокрутку тела страницы при открытом меню
         document.body.style.overflow = mobileMenu.classList.contains('active') ? 'hidden' : '';
     });
-    
-    // Функция закрытия меню
     function closeMenu() {
         menuBtn.classList.remove('active');
         mobileMenu.classList.remove('active');
         document.body.style.overflow = '';
     }
-    
-    // Для закрытия меню при переходе по ссылке внутри iframe
     window.addEventListener('message', function(e) {
         if (e.data === 'closeMobileMenu') {
         closeMenu();
@@ -62,14 +41,17 @@ document.addEventListener('DOMContentLoaded', function() {
     // Подсветка активной страницы
     const currentPage = window.location.pathname.split('/').pop();
     const menuLinks = document.querySelectorAll('.menu-link');
-    
+    const eventsSubpages = ['archive.html', 'archive.html#ifest-2025', 'archive.html#omut-autumn-2024'];
     menuLinks.forEach(link => {
         const linkPage = link.getAttribute('href');
-        if (linkPage === currentPage) {
+        const isEventsSubpage = eventsSubpages.some(page => 
+            currentPage === page || 
+            (page.includes('#') && currentPage === page.split('#')[0])
+        );
+        if (linkPage === currentPage || 
+            (linkPage === 'activ.html' && isEventsSubpage)) {
             link.classList.add('active');
         }
     });
-
-
 });
 
